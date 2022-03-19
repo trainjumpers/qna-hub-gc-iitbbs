00c39d606c26fb1ec1e955d9fa55cf24ce4204f3
Omkar <goa11@iitbbs.ac.in>
2022-03-19 14:43:53 +0530

    feat: authscreen UI * basic form setup for signup and login

--- src/views/Auth/Login.tsx
+++ src/views/Auth/Login.tsx
@@ -1,9 +1,46 @@
 import { FunctionComponent } from "react";
+import { Button, Card, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 interface LoginScreenProps {}
 
 const LoginScreen: FunctionComponent<LoginScreenProps> = () => {
-    return <div>Login</div>;
+    return (
+        <Container className="mt-5">
+            <Card style={{width: "45rem"}} className="mx-auto">
+                <Card.Header>
+                    <Card.Title>Login to your Account</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Group>
+                        <Button variant="primary">
+                            Login
+                        </Button>
+                    </Form>
+                </Card.Body>
+                <Card.Footer>
+                    <Form.Label>
+                        Don't have an account? Register{" "}
+                        <Link to="/signup">here</Link>
+                    </Form.Label>
+                </Card.Footer>
+            </Card>
+        </Container>
+    );
 };
 
 export default LoginScreen;
--- src/views/Auth/Signup.tsx
+++ src/views/Auth/Signup.tsx
@@ -1,9 +1,46 @@
 import { FunctionComponent } from "react";
+import { Button, Card, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 interface SignUpScreenProps {}
 
 const SignUpScreen: FunctionComponent<SignUpScreenProps> = () => {
-    return <div>Signup</div>;
+    return (
+        <Container className="mt-5">
+            <Card style={{ width: "45rem" }} className="mx-auto">
+                <Card.Header>
+                    <Card.Title>Register as a new User</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Group>
+                        <Button variant="primary">
+                            Signup
+                        </Button>
+                    </Form>
+                </Card.Body>
+                <Card.Footer>
+                    <Form.Label>
+                        Already an existing User? Log in{" "}
+                        <Link to="/login">here</Link>
+                    </Form.Label>
+                </Card.Footer>
+            </Card>
+        </Container>
+    );
 };
 
 export default SignUpScreen;
