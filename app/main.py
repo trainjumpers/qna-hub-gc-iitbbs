6c405785233fd6974883a3abaabe5c17aecb8dbc
Pranav <npv12@iitbbs.ac.in>
2022-03-20 00:59:43 +0530

    Allow all origins
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="GC Hackathon", version="1.0.0")
 
 app.include_router(router, prefix="/api")
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["localhost:3000"],
+                   allow_origins=["*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
